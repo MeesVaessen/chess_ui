7fc6ebfabaa3537b651231ed32e68d13fd2a9e2d
MeesVaessen <104736220+MeesVaessen@users.noreply.github.com>
2023-10-26 09:36:59 +0200

    chessboard

---
+++ user_ui/src/views/Lobby.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { TheChessboard } from 'vue3-chessboard';
+import 'vue3-chessboard/style.css';
+
+let boardApi;
+
+// Client will only be able to play white pieces.
+const playerColor = 'white';
+
+// Recieve move from socket/server/etc here.
+function onRecieveMove(move) {
+  boardApi?.move(move);
+}
+</script>
+
+<template>
+  <TheChessboard
+    :player-color="playerColor"
+    @board-created="(api) => (boardApi = api)"
+  />
+</template>
--- user_ui/vite.config.js
+++ user_ui/vite.config.js
@@ -5,6 +5,10 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: 'localhost',
+    port: 5173
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
